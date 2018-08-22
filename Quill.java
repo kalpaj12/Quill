ff393311e88633d9728fbe3e3f1983473df917a1
Kalpaj Agrawalla <kalpaj.agrawalla02@gmail.com>
2018-08-22 19:42:05 +0530

    Working Model

--- Quill.java
+++ Quill.java
@@ -14,6 +14,7 @@ import javafx.beans.property.*;
 	public static void main(String[] args) {
 	Application.launch(args);//args is already passed,change as required
 	}
+	
 	@Override
 	public void start(Stage stage) throws Exception {
 
